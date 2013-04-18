7a38665a9844b2c13986e91712404c3cadf0a435
Jack Lukic <jack@quirky.com>
2013-04-18 17:15:27 -0400

    ui: wrote a first pass at a menu, created an all new layout for docs with top fixed nav. wired in some spying menus that follow you around

--- docs/javascript/example.js
+++ docs/javascript/example.js
@@ -8,29 +8,45 @@ shape.ready = function() {
 
   // selector cache
   var 
-    $ui              = $('.ui'),
-    $swap            = $('.swap.button'),
-    $menu            = $('.menu.button'),
-    $demo            = $('.demo'),
+    $ui        = $('.ui'),
+    $swap      = $('.swap'),
+    $menu      = $('.menu.button'),
+    $demo      = $('.demo'),
+    $waypoints = $('h2'),
+    
+    $peek      = $('.peek'),
+    $peekMenu  = $peek.find('li'),
     // alias
     handler
   ;
 
   // event handlers
   handler = {
+
+    peek: function() {
+      $('html, body')
+        .animate({
+          scrollTop: $waypoints.eq( $peekMenu.index( $(this) ) ).offset().top - 90
+        }, 500, function(){
+          $(this).addClass('active').siblings().removeClass('active');
+        })
+      ;
+      $('html')
+        .one('scroll', function() {
+          $('html,body').stop();
+        })
+      ;
+    },
     swapStyle: function() {
-      $('head link')
+      $('head link.ui')
         .each(function() {
           var
             href = $(this).attr('href')
           ;
-          console.log (href, href.search('flat') );
           if( href.search('flat') > -1 ) {
-            console.log('zz');
             $(this).attr('href', href.replace('flat', 'shaded'));
           }
           else {
-            console.log('aaa');
             $(this).attr('href', href.replace('shaded', 'flat'));
           }
         })
@@ -43,6 +59,27 @@ shape.ready = function() {
     .state()
   ;
 
+  $waypoints
+    .waypoint({
+      continuous : false,
+      offset     : 220,
+      handler    : function(direction) {
+        var 
+          index = (direction == 'down')
+            ? $waypoints.index(this)
+            : ($waypoints.index(this) - 1 >= 0)
+              ? ($waypoints.index(this) - 1)
+              : 0
+        ;
+        $peekMenu
+          .removeClass('active')
+          .eq( index )
+            .addClass('active')
+        ;
+      }
+    })
+  ;
+
   $swap
     .on('click', handler.swapStyle)
   ;
@@ -51,6 +88,17 @@ shape.ready = function() {
     .sidr({
       name: 'menu'
     })
+  ;
+
+  $peek
+    .waypoint('sticky', {
+      offset: 80,
+      stuckClass: 'stuck'
+    })
+  ;
+  $peekMenu
+    .state()
+    .on('click', handler.peek)
   ;
 
 };
