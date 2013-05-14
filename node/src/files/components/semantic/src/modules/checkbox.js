57a6f24d95907c6722248e7a329b15e5ed51b0c5
Jack Lukic <jack@quirky.com>
2013-05-14 19:59:48 -0400

    adds back in spec, iteration on ui module order

--- node/src/files/components/semantic/src/modules/checkbox.js
+++ node/src/files/components/semantic/src/modules/checkbox.js
@@ -17,11 +17,20 @@ $.fn.checkbox = function(parameters) {
     $allModules     = $(this),
     
     settings        = $.extend(true, {}, $.fn.checkbox.settings, parameters),
-    // make arguments available
+
+    eventNamespace  = '.' + settings.namespace,
+    moduleNamespace = 'module-' + settings.namespace,
+
+    selector        = $allModules.selector || '',
+    time            = new Date().getTime(),
+    performance     = [],
+
     query           = arguments[0],
-    passedArguments = [].slice.call(arguments, 1),
+    methodInvoked   = (typeof query == 'string'),
+    queryArguments  = [].slice.call(arguments, 1),
     invokedResponse
   ;
+
   $allModules
     .each(function() {
       var
@@ -31,7 +40,6 @@ $.fn.checkbox = function(parameters) {
         selector      = $module.selector || '',
         element       = this,
         instance      = $module.data('module-' + settings.namespace),
-        methodInvoked = (typeof query == 'string'),
         
         className     = settings.className,
         namespace     = settings.namespace,
@@ -45,13 +53,15 @@ $.fn.checkbox = function(parameters) {
           if(settings.context && selector !== '') {
             module.verbose('Initializing checkbox with delegated events', $module);
             $(element, settings.context)
-              .on(selector, 'click.' + namespace, module.toggle)
+              .on(selector, 'click' + eventNamespace, module.toggle)
+              .data(moduleNamespace, module)
             ;
           }
           else {
             module.verbose('Initializing checkbox with bound events', $module);
             $module
-              .on('click.' + namespace, module.toggle)
+              .on('click' + eventNamespace, module.toggle)
+              .data(moduleNamespace, module)
             ;
           }
         },
@@ -71,20 +81,7 @@ $.fn.checkbox = function(parameters) {
             module.disable();
           }
         },
-
-        enableAll: function() {
-          module.debug('Disabling all checkbox');
-          $.each($allModules, function() {
-            $(this).checkbox('enable');
-          });
-        },
-        disableAll: function() {
-          module.debug('Enabling all checkbox');
-          $.each($allModules, function() {
-            $(this).checkbox('disable');
-          });
-        },
-
+        
         enable: function() {
           module.debug('Enabling checkbox');
           $module
@@ -109,51 +106,123 @@ $.fn.checkbox = function(parameters) {
           $.proxy(settings.onDisable, $input.get())();
         },
 
-        /* standard module */
+        all: {
+          enable: function() {
+            module.debug('Enabling all checkbox');
+            $allModules
+              .checkbox('enable')
+            ;
+          },
+          disable: function() {
+            module.debug('Disabling all checkbox');
+            $allModules
+              .checkbox('toggle')
+            ;
+          },
+          toggle: function() {
+            module.debug('Toggling all checkbox');
+            $allModules
+              .checkbox('toggle')
+            ;
+          }
+        },
+        
         setting: function(name, value) {
-          if(value === undefined) {
+          if(value !== undefined) {
+            if( $.isPlainObject(name) ) {
+              $.extend(true, settings, name);
+            }
+            else {
+              settings[name] = value;
+            }
+          }
+          else {
             return settings[name];
           }
-          settings[name] = value;
         },
-        verbose: function() {
-          if(settings.verbose) {
-            module.debug.apply(this, arguments);
+        internal: function(name, value) {
+          if(value !== undefined) {
+            if( $.isPlainObject(name) ) {
+              $.extend(true, module, name);
+            }
+            else {
+              module[name] = value;
+            }
+          }
+          else {
+            return module[name];
           }
         },
         debug: function() {
-          var
-            output    = [],
-            message   = settings.moduleName + ': ' + arguments[0],
-            variables = [].slice.call( arguments, 1 ),
-            log       = console.info || console.log || function(){}
-          ;
-          log = Function.prototype.bind.call(log, console);
           if(settings.debug) {
-            output.push(message);
-            log.apply(console, output.concat(variables) );
+            module.performance.log(arguments[0]);
+            module.verbose = Function.prototype.bind.call(console.info, console, settings.moduleName + ':');
+          }
+        },
+        verbose: function() {
+          if(settings.verbose && settings.debug) {
+            module.performance.log(arguments[0]);
+            module.verbose = Function.prototype.bind.call(console.info, console, settings.moduleName + ':');
           }
         },
         error: function() {
-          var
-            output       = [],
-            errorMessage = settings.moduleName + ': ' + arguments[0],
-            variables    = [].slice.call( arguments, 1 ),
-            log          = console.warn || console.log || function(){}
-          ;
-          log = Function.prototype.bind.call(log, console);
-          if(settings.debug) {
-            output.push(errorMessage);
-            output.concat(variables);
-            log.apply(console, output.concat(variables) );
+          if(console.log !== undefined) {
+            module.error = Function.prototype.bind.call(console.log, console, settings.moduleName + ':');
           }
         },
-        invoke: function(query, context, passedArguments) {
+        performance: {
+          log: function(message) {
+            var
+              currentTime,
+              executionTime,
+              previousTime
+            ;
+            if(settings.performance) {
+              currentTime   = new Date().getTime();
+              previousTime  = time || currentTime,
+              executionTime = currentTime - previousTime;
+              time          = currentTime;
+              performance.push({ 
+                'Name'           : message, 
+                'Execution Time' : executionTime
+              });
+              clearTimeout(module.performance.timer);
+              module.performance.timer = setTimeout(module.performance.display, 100);
+            }
+          },
+          display: function() {
+            var
+              title              = settings.moduleName + ' Performance (' + selector + ')',
+              caption            = settings.moduleName + ': ' + selector + '(' + $allModules.size() + ' elements)',
+              totalExecutionTime = 0
+            ;
+            if( (console.group !== undefined || console.table !== undefined) && performance.length > 0) {
+              console.groupCollapsed(title );
+              if(console.table) {
+                $.each(performance, function(index, data) {
+                  totalExecutionTime += data['Execution Time'];
+                });
+                console.table(performance);
+              }
+              else {
+                $.each(performance, function(index, data) {
+                  totalExecutionTime += data['Execution Time'];
+                });
+              }
+              console.log('Total Execution Time:', totalExecutionTime);
+              console.groupEnd();
+              performance = [];
+              time        = false;
+            }
+          }
+        },
+        invoke: function(query, passedArguments, context) {
           var
             maxDepth,
             found
           ;
-          passedArguments = passedArguments || [].slice.call( arguments, 2 );
+          passedArguments = passedArguments || queryArguments;
+          context         = element         || context;
           if(typeof query == 'string' && instance !== undefined) {
             query    = query.split('.');
             maxDepth = query.length - 1;
@@ -166,31 +235,35 @@ $.fn.checkbox = function(parameters) {
                 found = instance[value];
                 return true;
               }
-              module.error(errors.method);
+              module.error(error.method);
               return false;
             });
           }
           if ( $.isFunction( found ) ) {
+            module.verbose('Executing invoked function', found);
             return found.apply(context, passedArguments);
           }
-          // return retrieved variable or chain
-          return found;
+          return found || false;
         }
       };
 
       // check for invoking internal method
       if(methodInvoked) {
-        invokedResponse = module.invoke(query, this, passedArguments);
+        if(instance === undefined) {
+          module.initialize();
+        }
+        invokedResponse = module.invoke(query);
       }
-      // otherwise initialize
       else {
+        if(instance !== undefined) {
+          module.destroy();
+        }
         module.initialize();
       }
-
     })
   ;
   // chain or return queried method
-  return (invokedResponse !== undefined)
+  return (invokedResponse)
     ? invokedResponse
     : this
   ;
