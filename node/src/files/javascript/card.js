3abbe00622cd451bd44c683b64045b969a068812
Jack Lukic <jack@quirky.com>
2013-06-13 18:00:13 -0400

    Updates cards
    Former-commit-id: 9d990029b7c86f30b58bca25ca660c4be37f0b99
    Former-commit-id: a0813e597eac431e7f30b770669c182073219a26

--- node/src/files/javascript/card.js
+++ node/src/files/javascript/card.js
@@ -5,10 +5,26 @@ semantic.card.ready = function() {
 
   // selector cache
   var 
-    $card = $('.ui.idea.cards .card, .ui.idea.card')
+    $card = $('.ui.idea.cards .card, .ui.idea.card'),
+    handler
   ;
 
+  handler = {
+
+    randomProgress: function(index) {
+      var $this = $(this);
+      setTimeout(function() {
+        $this
+          .find('.bar')
+            .css('width', Math.floor(Math.random() * 100) + '%')
+        ;
+      }, index * 200);
+    }
+
+  };
+
   $card
+    .each(handler.randomProgress)
     .card()
   ;
   
