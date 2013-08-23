bf06cebc8ba55c19b0d835f2f2fb16199e4c4997
jlukic <jacklukic@gmail.com>
2013-08-23 15:13:17 -0400

    Updates to header, button, and lots of updates to dropdown
    Former-commit-id: 1bcb227595bec152d69e914091e56d5cdc37440a
    Former-commit-id: bf9b8adc54952230d2d7c1bf899cb1299a438c85

--- node/src/files/javascript/dropdown.js
+++ node/src/files/javascript/dropdown.js
@@ -4,19 +4,36 @@ semantic.dropdown = {};
 semantic.dropdown.ready = function() {
 
   // selector cache
-  var 
-    $examples      = $('.example'),
-    $hoverDropdown = $examples.filter('.hover').find('.ui.dropdown'),
-    $formDropdown  = $examples.filter('.form').find('.ui.dropdown'),
-    $dropdown      = $examples.filter('.dropdown').find('.ui.dropdown:not(.simple)'),
+  var
+    $examples         = $('.example'),
+    $hoverDropdown    = $examples.filter('.hover').find('.ui.dropdown'),
+    $formDropdown     = $examples.filter('.form').find('.ui.dropdown'),
+    $dropdown         = $examples.filter('.dropdown').find('.ui.dropdown:not(.simple)'),
+    $transition       = $examples.filter('.transition').find('.ui.dropdown'),
+    $transitionButton = $examples.filter('.transition').find('.ui.button').first(),
     // alias
     handler
   ;
 
   // event handlers
   handler = {
-    
+
   };
+
+  $transitionButton
+    .on('click', function(event) {
+      $transition.dropdown('toggle');
+      event.stopImmediatePropagation();
+    })
+  ;
+
+  $transition
+    .dropdown({
+      onChange: function(value) {
+        $transition.dropdown('setting', 'transition', value);
+      }
+    })
+  ;
 
   $dropdown
     .dropdown({
@@ -33,7 +50,7 @@ semantic.dropdown.ready = function() {
       on: 'hover'
     })
   ;
-  
+
 };
 
 
