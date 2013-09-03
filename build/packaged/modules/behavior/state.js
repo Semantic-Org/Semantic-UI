cd9423827ef6947eec84eedf8a6a67a3d3253f8f
jlukic <jacklukic@gmail.com>
2013-09-02 20:46:02 -0400

    weekend updates
    Former-commit-id: e98a244e5221a7d17ad8141b2c081808f249b604
    Former-commit-id: eb92f8f7cb4f812288b3b3304021048f80342a5a

--- build/packaged/modules/behavior/state.js
+++ build/packaged/modules/behavior/state.js
@@ -493,7 +493,7 @@ $.fn.state = function(parameters) {
               module.performance.log(arguments);
             }
             else {
-              module.debug = Function.prototype.bind.call(console.info, console, settings.moduleName + ':');
+              module.debug = Function.prototype.bind.call(console.info, console, settings.name + ':');
               module.debug.apply(console, arguments);
             }
           }
@@ -504,13 +504,13 @@ $.fn.state = function(parameters) {
               module.performance.log(arguments);
             }
             else {
-              module.verbose = Function.prototype.bind.call(console.info, console, settings.moduleName + ':');
+              module.verbose = Function.prototype.bind.call(console.info, console, settings.name + ':');
               module.verbose.apply(console, arguments);
             }
           }
         },
         error: function() {
-          module.error = Function.prototype.bind.call(console.error, console, settings.moduleName + ':');
+          module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
           module.error.apply(console, arguments);
         },
         performance: {
