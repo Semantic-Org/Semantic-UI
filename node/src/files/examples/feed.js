d4ea578e2f8e87d295c7596a395b2159583fb90d
jlukic <jacklukic@gmail.com>
2013-09-15 13:10:23 -0400

    typo in js

--- node/src/files/examples/feed.js
+++ node/src/files/examples/feed.js
@@ -1,26 +1,30 @@
 $(document)
   .ready(function() {
 
-    var setActive = function(event) {
-      $(this)
-        .addClass('active')
-        .closest('.ui.menu')
-          .find('.item').not(this)
-          .removeClass('active')
-      ;
-      event.stopPropagation();
-    };
-
     $('.filter.menu .item')
       .tab()
     ;
 
     $('.left.column .menu .item')
-      .on('click',  setActive)
+      .on('click', function() {
+        $(this)
+          .addClass('active')
+          .closest('.ui.menu')
+            .find('.item').not(this)
+            .removeClass('active')
+        ;
+        event.stopPropagation();
+      })
     ;
 
     $('.list .item')
-      .on('click',  setActive)
+      .on('click',  function() {
+        $(this)
+          .addClass('active')
+          .siblings()
+          .removeClass('active')
+        ;
+      })
     ;
 
   })
