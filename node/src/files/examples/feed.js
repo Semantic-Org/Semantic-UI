4d8466cad35082aeca7bd063c726e0f18211c9e7
jlukic <jacklukic@gmail.com>
2013-09-15 13:07:13 -0400

    feed: adds more fake js to example

--- node/src/files/examples/feed.js
+++ node/src/files/examples/feed.js
@@ -1,8 +1,26 @@
 $(document)
   .ready(function() {
 
+    var setActive = function(event) {
+      $(this)
+        .addClass('active')
+        .closest('.ui.menu')
+          .find('.item').not(this)
+          .removeClass('active')
+      ;
+      event.stopPropagation();
+    };
+
     $('.filter.menu .item')
       .tab()
+    ;
+
+    $('.left.column .menu .item')
+      .on('click',  setActive)
+    ;
+
+    $('.list .item')
+      .on('click',  setActive)
     ;
 
   })
