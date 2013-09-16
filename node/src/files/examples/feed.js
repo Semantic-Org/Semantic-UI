43cb5e4285bc41b899f7b6e492cdeb18b8bdb538
jlukic <jacklukic@gmail.com>
2013-09-16 18:17:33 -0400

    example fix

--- node/src/files/examples/feed.js
+++ node/src/files/examples/feed.js
@@ -5,27 +5,5 @@ $(document)
       .tab()
     ;
 
-    $('.left.column .menu .item')
-      .on('click', function() {
-        $(this)
-          .addClass('active')
-          .closest('.ui.menu')
-            .find('.item').not(this)
-            .removeClass('active')
-        ;
-        event.stopPropagation();
-      })
-    ;
-
-    $('.list .item')
-      .on('click',  function() {
-        $(this)
-          .addClass('active')
-          .siblings()
-          .removeClass('active')
-        ;
-      })
-    ;
-
   })
 ;
