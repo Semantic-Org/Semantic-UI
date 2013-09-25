b96e8628eb85aa0a0595a1db8f9a9b1f2e586f84
jlukic <jacklukic@gmail.com>
2013-09-24 20:11:44 -0400

    fixes work on importing font awesome icon
    Former-commit-id: 6528e04cd5d67e4d2307d08b8dbbf8729bf7b791
    Former-commit-id: 0e0814fb0bfc7426d25a8cdd43eb0085cdefa4e4

---
+++ node/src/files/javascript/icon.js
@@ -0,0 +1,28 @@
+semantic.shape = {};
+
+// ready event
+semantic.shape.ready = function() {
+
+  // selector cache
+  var
+    $tab = $('.tabular.menu .item'),
+    handler
+  ;
+
+  // event handlers
+  handler = {
+
+  };
+
+
+  $tab
+    .tab()
+  ;
+
+};
+
+
+// attach ready event
+$(document)
+  .ready(semantic.shape.ready)
+;
