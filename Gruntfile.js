69ffc10a9b8b94e08230a7f0163e902e2490b517
makotot <makotottn@gmail.com>
2013-11-28 23:59:29 +0900

    use load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -611,20 +611,7 @@ module.exports = function(grunt) {
   };
 
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-docco-multi');
-  grunt.loadNpmTasks('grunt-cssjanus');
-  grunt.loadNpmTasks('grunt-clear');
-  grunt.loadNpmTasks('grunt-karma');
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig(config);
 
