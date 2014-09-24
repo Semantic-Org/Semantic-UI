0947740fd520a7a8685b4f19559fecdc55487f0d
jlukic <jacklukic@gmail.com>
2014-09-24 11:26:36 -0400

    Adds indicating progress bar

--- server/files/javascript/progress.js
+++ server/files/javascript/progress.js
@@ -4,11 +4,38 @@ semantic.progress = {};
 semantic.progress.ready = function() {
 
   var
-    $progress      = $('.definition  .ui.progress').not('.success, .error, .warning'),
-    $stateProgress = $('.definition .ui.success.progress, .ui.warning.progress, .ui.error.progress')
+    $progress         = $('.definition  .ui.progress').not('.success, .error, .warning, .indicating'),
+    $indicating       = $('.definition .ui.indicating.progress'),
+    $indicatingButton = $('.definition .indicating.example .button'),
+    $stateProgress    = $('.definition .ui.success.progress, .ui.warning.progress, .ui.error.progress')
   ;
 
   setTimeout(function() {
+
+    $indicatingButton
+      .on('click', function() {
+        var
+          $progress = $(this).closest('.example').find('.progress')
+        ;
+        if( $(this).hasClass('increment') ) {
+          $progress.progress('increment');
+        }
+        else {
+          $progress.progress('decrement');
+        }
+      })
+    ;
+
+    $indicating
+      .progress({
+        label : false,
+        total : 10,
+        text  : {
+          active: '{percent}% Funded',
+          success: 'Project Funded!'
+        }
+      })
+    ;
 
     $progress
       .progress({
