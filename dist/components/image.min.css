bf8adfb42d96b39ffe06e8e7d137eb9f41092d56
jlukic <jacklukic@gmail.com>
2014-10-31 18:33:30 -0400

    Whitespace in comment banners

--- dist/components/image.min.css
+++ dist/components/image.min.css
@@ -3,7 +3,6 @@
  * https://github.com/Semantic-Org/Semantic-UI
  * http://beta.semantic-ui.com/
  *
- *
  * Copyright 2014 Contributors
  * Released under the MIT license
  * http://opensource.org/licenses/MIT
