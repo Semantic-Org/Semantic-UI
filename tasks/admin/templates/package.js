db25fb6f166beab19df01f48d360cb5b2cdad4db
jlukic <jacklukic@gmail.com>
2015-01-12 13:37:11 -0500

    #1607 formatting

--- tasks/admin/templates/package.js
+++ tasks/admin/templates/package.js
@@ -1,18 +1,18 @@
-Package.describe({
-  name: 'semantic:ui',
-  summary: 'Semantic (official): a UI component framework based around useful principles from natural language.',
-  version: '{package-version}',
-  git: 'git://github.com/Semantic-Org/Semantic-UI.git',
-  readme: 'https://github.com/Semantic-Org/Semantic-UI/blob/master/meteor/README.md'
-});
+var
+  where = 'client' // Adds files only to the client
+;
 
-var where = 'client'; // Adds files only to the client
+Package.describe({
+  name    : 'semantic:ui',
+  summary : 'Semantic (official): a UI component framework based around useful principles from natural language.',
+  version : '{package-version}',
+  git     : 'git://github.com/Semantic-Org/Semantic-UI.git',
+  readme  : 'https://github.com/Semantic-Org/Semantic-UI/blob/master/meteor/README.md'
+});
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-
-  api.addFiles([
-{package-files}  ], where);
+  api.addFiles([ {package-files} ], where);
 });
 
 Package.onTest(function(api) {
