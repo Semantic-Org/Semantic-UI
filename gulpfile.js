41b0c8b093b278f4d4edb9fc4725f378b16f3e8a
jlukic <jacklukic@gmail.com>
2015-01-15 11:24:16 -0500

    Remove RTL debug

--- gulpfile.js
+++ gulpfile.js
@@ -864,7 +864,7 @@ gulp.task('install', 'Set-up project for first time', function () {
         };
       }
       if(answers.rtl) {
-        json.rtl = true;
+        json.rtl = answers.rtl;
       }
       if(answers.site) {
         json.paths.source.site = answers.site + '/';
