3b7d554de6cd930a44a716d5b704690721b96e4b
jlukic <jacklukic@gmail.com>
2015-02-10 16:54:37 -0500

    More debug

--- tasks/install.js
+++ tasks/install.js
@@ -130,6 +130,8 @@ module.exports = function () {
         return;
       }
       else {
+        console.log(release);
+        console.log(requireDotFile('package.json'));
         console.log('Current version of Semantic UI already installed, skipping set-up');
         return;
       }
