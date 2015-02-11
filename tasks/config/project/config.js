388a11bb1499d01ad2604290b5b285d0c7ecbbcd
jlukic <jacklukic@gmail.com>
2015-02-11 17:02:56 -0500

    Fix typo in config

--- tasks/config/project/config.js
+++ tasks/config/project/config.js
@@ -73,7 +73,7 @@ module.exports = {
     for(folder in config.paths.output) {
       if(config.paths.output.hasOwnProperty(folder)) {
         // full path is (config location + base + path)
-        config.paths.source[folder] = path.resolve(path.join(configPath, config.base, config.paths.output[folder]));
+        config.paths.output[folder] = path.resolve(path.join(configPath, config.base, config.paths.output[folder]));
       }
     }
 
