ec415f500b4b68ddc46993202ba191da37f166b7
jlukic <jacklukic@gmail.com>
2015-02-23 15:12:10 -0500

    Update css package for meteor #1607

--- tasks/config/admin/templates/css-package.js
+++ tasks/config/admin/templates/css-package.js
@@ -14,6 +14,19 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   api.addFiles([
+    // icons
+    'themes/default/assets/fonts/icons.eot',
+    'themes/default/assets/fonts/icons.otf',
+    'themes/default/assets/fonts/icons.svg',
+    'themes/default/assets/fonts/icons.ttf',
+    'themes/default/assets/fonts/icons.woff',
+    'themes/default/assets/fonts/icons.woff2',
+    'themes/default/assets/fonts/icons.woff2',
+
+    // flags
+    'themes/default/assets/images/flags.png',
+
+    // release
     'semantic.css',
     'semantic.js'
   ], 'client');
