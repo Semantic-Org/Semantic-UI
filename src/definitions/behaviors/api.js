d3b43154138a5bd697cd3011aad79d002562b41a
jlukic <jacklukic@gmail.com>
2015-02-28 11:47:49 -0500

    #1889 serializeForm produces unnecessary error

--- src/definitions/behaviors/api.js
+++ src/definitions/behaviors/api.js
@@ -549,7 +549,7 @@ $.api = $.fn.api = function(parameters) {
             var
               formData
             ;
-            if($(this).serializeObject() !== undefined) {
+            if($(this).serializeObject !== undefined) {
               formData = $form.serializeObject();
             }
             else {
