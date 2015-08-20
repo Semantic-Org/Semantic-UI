6eda430d3cecb6c06ba710794d1ee1cf76ce8dc3
tvildo <davidtamuna@gmail.com>
2015-08-21 02:30:14 +0400

    Fix Add Errors Should Display Immediately #1172
    fixes annoying issue :  https://github.com/Semantic-Org/Semantic-UI/issues/1172

--- src/definitions/behaviors/form.js
+++ src/definitions/behaviors/form.js
@@ -557,6 +557,7 @@ $.fn.form = function(parameters) {
           },
           errors: function(errors) {
             module.debug('Adding form error messages', errors);
+            module.set.error();
             $message
               .html( settings.templates.error(errors) )
             ;
