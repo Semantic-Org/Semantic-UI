641feb44e223af7853e0d032a9761b2193bfbecd
Machisté N. Quintana <mnquintana@users.noreply.github.com>
2015-10-05 15:05:49 -0400

    Call onSearchQuery whether or not results are cached
    Fixes https://github.com/Semantic-Org/Semantic-UI/issues/3106

--- src/definitions/modules/search.js
+++ src/definitions/modules/search.js
@@ -424,8 +424,8 @@ $.fn.search = function(parameters) {
               else {
                 module.error(error.source);
               }
-              settings.onSearchQuery.call(element, searchTerm);
             }
+            settings.onSearchQuery.call(element, searchTerm);
           }
           else {
             module.hideResults();
