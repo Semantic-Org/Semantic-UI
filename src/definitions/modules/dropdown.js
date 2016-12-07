634d2e0a08d0f905be4b6d6f7c3f025f474eed1c
Enix Yu <enix223@163.com>
2016-12-07 10:46:40 +0800

    dropdown localsearch default to false

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -3504,7 +3504,7 @@ $.fn.dropdown.settings = {
 
 
   apiSettings            : false,
-  localSearch            : true,       // Whether trigger local search instead of querying data from remote source
+  localSearch            : false,      // Whether trigger local search instead of querying data from remote source
   selectOnKeydown        : true,       // Whether selection should occur automatically when keyboard shortcuts used
   minCharacters          : 0,          // Minimum characters required to trigger API call
   saveRemoteData         : true,       // Whether remote name/value pairs should be stored in sessionStorage to allow remote data to be restored on page refresh
