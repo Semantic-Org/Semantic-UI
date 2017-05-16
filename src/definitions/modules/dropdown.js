d1ad60ddc5c52bda202853ce85448e0c85b8e99f
Andreas Fransson <root@zykadelic.com>
2017-05-16 12:16:30 +0200

    removes addedValue from call to module.set.value, fixes Semantic-Org/Semantic-UI#2888

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -2710,7 +2710,7 @@ $.fn.dropdown = function(parameters) {
             else {
               settings.onAdd.call(element, addedValue, addedText, $selectedItem);
             }
-            module.set.value(newValue, addedValue, addedText, $selectedItem);
+            module.set.value(newValue, addedText, $selectedItem);
             module.check.maxSelections();
           }
         },
