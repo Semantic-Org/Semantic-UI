295af5f328c5072e779d6ad4a0caec24509d4294
Jack <jacklukic@gmail.com>
2018-03-18 16:13:34 -0700

    Fix build-docs task not requiring component name in filename #6226

--- tasks/docs/build.js
+++ tasks/docs/build.js
@@ -162,7 +162,7 @@ module.exports = function(callback) {
   ;
 
   // copy assets
-  gulp.src(source.themes + '/**/assets/**/' + globs.components + '?(s).*')
+  gulp.src(source.themes + '/**/assets/**/*.*')
     .pipe(gulpif(config.hasPermission, chmod(config.permission)))
     .pipe(gulp.dest(output.themes))
   ;
