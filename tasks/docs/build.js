bcc083976da48b20c5155b17329511550b134ecc
Adam Conrad <acconrad@protonmail.com>
2018-10-01 10:40:51 -0400

    fix build docs issue

--- tasks/docs/build.js
+++ tasks/docs/build.js
@@ -80,7 +80,11 @@ module.exports = function(callback) {
   gulp.src(config.paths.template.eco + globs.eco)
     .pipe(map(metadata.parser))
     .on('end', function() {
-      fs.writeFile(output.metadata + '/metadata.json', JSON.stringify(metadata.result, null, 2));
+      fs.writeFile(
+        output.metadata + '/metadata.json',
+        JSON.stringify(metadata.result, null, 2),
+        function (err) { err && console.error(err) }
+      );
     })
   ;
 
